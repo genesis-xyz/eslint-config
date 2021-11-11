f72305872b31edccb6bc508355a390a69621e1d7
Dan Scanlon <1638987+danscan@users.noreply.github.com>
2021-11-11 10:49:30 -0500

    Update and comment config for Genesis

--- index.js
+++ index.js
@@ -19,25 +19,24 @@ module.exports = {
     '@typescript-eslint/no-use-before-define': 0,
     '@typescript-eslint/no-var-requires': 0,
     'import/extensions': 0,
-    'import/no-extraneous-dependencies': 0,
+    'import/no-extraneous-dependencies': 'warn',
     'jsx-a11y/anchor-is-valid': 0,
-    'max-len': 0,
-    'no-nested-ternary': 0,
+    'max-len': 0, // Handled by prettier
     'no-use-before-define': 0,
-    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/exhaustive-deps': 'error',
     'react-hooks/rules-of-hooks': 'error',
     'react/jsx-curly-newline': 0,
     'react/jsx-one-expression-per-line': 0,
     'react/jsx-props-no-spreading': 'warn',
-    'react/no-unescaped-entities': 0,
+    'react/no-unescaped-entities': 0, // N/A to React Native
     'react/prop-types': [1, { skipUndeclared: true }],
-    'spaced-comment': 0,
+    'spaced-comment': ['error', 'always', { exceptions: ['-', '*', '='] }],
 
     // Prettier
     'prettier/prettier': [
       'error',
       {
-        printWidth: 80,
+        printWidth: 120,
         semi: true,
         singleQuote: true,
         trailingComma: 'es5',
